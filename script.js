8d3fc57d9931a8cb545cdea7ae665d94cbe62732
ByteTabs1 <157298649+ByteTabs1@users.noreply.github.com>
2024-06-07 14:44:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -57,14 +57,14 @@
   $(document).ready(function () {
 
 
-    // JavaScript For SideBar DropDown
+    // JQuery For SideBar DropDown
   
     $('.sub-btn').click(function () {
       $(this).next('.sub-menu').slideToggle();
       $(this).find('.dropdown').toggleClass('rotate');
     });
   
-    // JavaScript For ScrollBar DropDown
+    // JQuery For ScrollBar DropDown
   
     $('.nav-dropdown').hover(function () {
       $(this).find('.nav-dropdown-content').stop(true, true).slideDown();
@@ -73,28 +73,28 @@
     });
   
   
-    // JavaScript For Showing SideBar Using Down Hamburger
+    // JQuery For Showing SideBar Using Down Hamburger
   
     $('.hamburger').click(function () {
       $('.side-bar').addClass('active');
       $('.hamburger').css("visibility", "hidden");
     });
   
-    // JavaScript For Closing SideBar Using Down Hamburger
+    // JQuery For Closing SideBar Using Down Hamburger
   
     $('.close-btn').click(function () {
       $('.side-bar').removeClass('active');
       $('.hamburger').css("visibility", "visible");
     });
   
-    // JavaScript For Showing SideBar Using Top Hamburger
+    // JQuery For Showing SideBar Using Top Hamburger
   
     $('.hamburger-top').click(function () {
       $('.side-bar').addClass('active');
       $('.hamburger-top').css("visibility", "hidden");
     });
   
-    // JavaScript For Closing SideBar Using Top Hamburger
+    // JQuery For Closing SideBar Using Top Hamburger
   
     $('.close-btn').click(function () {
       $('.side-bar').removeClass('active');
