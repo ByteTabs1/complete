59cbeb57d57e945487da4579a66799fcb797fe7c
ByteTabs1 <157298649+ByteTabs1@users.noreply.github.com>
2024-06-07 14:41:36 +0530

    Update script.js

--- lounge Interview/script.js
+++ lounge Interview/script.js
@@ -1,14 +1,14 @@
   $(document).ready(function () {
 
 
-    // JavaScript For SideBar DropDown
+    // JQuery For SideBar DropDown
   
     $('.sub-btn').click(function () {
       $(this).next('.sub-menu').slideToggle();
       $(this).find('.dropdown').toggleClass('rotate');
     });
   
-    // JavaScript For ScrollBar DropDown
+    // JQuery For ScrollBar DropDown
   
     $('.nav-dropdown').hover(function () {
       $(this).find('.nav-dropdown-content').stop(true, true).slideDown();
@@ -17,46 +17,46 @@
     });
   
   
-    // JavaScript For Showing SideBar Using Down Hamburger
+    // JQuery For Showing SideBar Using Down Hamburger
   
     $('.hamburger').click(function () {
       $('.side-bar').addClass('active');
       $('.hamburger').css("visibility", "hidden");
     });
   
-    // JavaScript For Closing SideBar Using Down Hamburger
+    // JQuery For Closing SideBar Using Down Hamburger
   
     $('.close-btn').click(function () {
       $('.side-bar').removeClass('active');
       $('.hamburger').css("visibility", "visible");
     });
   
-    // JavaScript For Showing SideBar Using Top Hamburger
+    // JQuery For Showing SideBar Using Top Hamburger
   
     $('.hamburger-top').click(function () {
       $('.side-bar').addClass('active');
       $('.hamburger-top').css("visibility", "hidden");
     });
   
-    // JavaScript For Closing SideBar Using Top Hamburger
+    // JQuery For Closing SideBar Using Top Hamburger
   
     $('.close-btn').click(function () {
       $('.side-bar').removeClass('active');
       $('.hamburger-top').css("visibility", "visible");
     });
   
-    // JavaScript For Load More Content
+    // JQuery For Load More Content
   
-    $(".abc").slice(0, 6).show(); // Initially show the first 8 boxes
-    var itemsToShow = 3; // Number of items to show on each click
+    $(".abc").slice(0, 6).show(); 
+    var itemsToShow = 3; 
     $(".load-more").on('click', function (e) {
         e.preventDefault();
         var hiddenItems = $(".abc:hidden").slice(0, itemsToShow);
-        hiddenItems.show(); // Show next 4 hidden boxes
+        hiddenItems.show(); 
         if ($(".abc:hidden").length === 0) {
             $(".load-more").css('visibility', 'hidden');
         }
-        // Scroll to the last of the newly displayed elements
+       
         $('html,body').animate({
             scrollTop: hiddenItems.last().offset().top
         }, 1000);
